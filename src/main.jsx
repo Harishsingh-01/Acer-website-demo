f4863c22019863ea13f0e20a295ed3a8b8c5efb5
harishsingh-01 <harishchaudhary790@gmail.com>
2026-02-16 16:47:38 +0530

    feat: Initialize React application and configure production API base URL using environment variables.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
+import axios from 'axios';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+// Set base URL for production
+if (import.meta.env.VITE_API_URL) {
+    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+    axios.defaults.withCredentials = true;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
